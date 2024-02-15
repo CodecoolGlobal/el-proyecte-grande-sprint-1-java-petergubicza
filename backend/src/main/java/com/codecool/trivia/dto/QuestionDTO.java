621a49edfa69f1f99df93584e7a89c719af7f03e
Peter Gubicza <132732117+petergubicza@users.noreply.github.com>
2024-02-15 22:07:47 +0100

    refactored questionDTO and connected classes, now it returns question and answers with teir id

--- backend/src/main/java/com/codecool/trivia/dto/QuestionDTO.java
+++ backend/src/main/java/com/codecool/trivia/dto/QuestionDTO.java
@@ -1,4 +1,23 @@
 package com.codecool.trivia.dto;
 
-public record QuestionDTO(String id, String question, String category, String difficulty, String[] answers) {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class QuestionDTO {
+    private String questionDescription;
+    private List<AnswerDTO> answers; // Map<Answer description, Answer id>
+
+    public QuestionDTO(String questionDescription, List<AnswerDTO> answers) {
+        this.questionDescription = questionDescription;
+        this.answers = new ArrayList<>(answers);
+    }
+
+    public String getQuestionDescription() {
+        return questionDescription;
+    }
+
+    public List<AnswerDTO> getAnswers() {
+        return answers;
+    }
 }
--- backend/src/main/java/com/codecool/trivia/service/RandomQuestionService.java
+++ backend/src/main/java/com/codecool/trivia/service/RandomQuestionService.java
@@ -1,11 +1,14 @@
 package com.codecool.trivia.service;
 
+import com.codecool.trivia.dto.AnswerDTO;
 import com.codecool.trivia.dto.QuestionDTO;
 import com.codecool.trivia.exception.NotFoundQuestionException;
 import com.codecool.trivia.model.entity.Question;
 import com.codecool.trivia.service.random_question.RandomQuestionGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RandomQuestionService {
@@ -19,13 +22,10 @@ public class RandomQuestionService {
     public QuestionDTO getRandomQuestionDTO() {
         Question randomQuestion = this.randomQuestionGenerator.getRandomQuestion();
         try {
-            String[] answersForQuestion = this.randomQuestionGenerator.getAnswersForCertainQuestion(randomQuestion);
+            List<AnswerDTO> answers = this.randomQuestionGenerator.getAnswersForCertainQuestion(randomQuestion);
             return new QuestionDTO(
-                    randomQuestion.getId().toString(),
                     randomQuestion.getQuestionDescription(),
-                    randomQuestion.getCategory().toString(),
-                    randomQuestion.getDifficulty().toString(),
-                    answersForQuestion
+                    answers
             );
         } catch (NotFoundQuestionException e) {
             throw new RuntimeException(e.getMessage());
