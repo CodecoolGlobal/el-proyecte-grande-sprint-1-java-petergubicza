d4f432ea7458149d03be724e2eb9f93d1e2d61b2
Intcognito <130778504+Intcognito@users.noreply.github.com>
2024-02-15 09:25:40 +0100

    refactored leaderboard controller and service by moving DTO creation to service

--- backend/src/main/java/com/codecool/trivia/controller/LeaderboardController.java
+++ backend/src/main/java/com/codecool/trivia/controller/LeaderboardController.java
@@ -21,16 +21,6 @@ public class LeaderboardController {
 
   @GetMapping(value = "/leaderboard")
   public LeaderboardDTO fetchTopFivePlayers() {
-    List<TriviaUser> topPlayers = leaderboardService.findTopFiveUsers();
-
-    String[] players = new String[LEADERBOARD_LENGTH];
-    double[] points = new double[LEADERBOARD_LENGTH];
-
-    for (int i = 0; i < topPlayers.size(); i++) {
-      players[i] = topPlayers.get(i).getName();
-      points[i] = topPlayers.get(i).getPoints();
-    }
-
-    return new LeaderboardDTO(players, points);
+    return leaderboardService.findTopFiveUsers();
   }
 }
--- backend/src/main/java/com/codecool/trivia/service/LeaderboardService.java
+++ backend/src/main/java/com/codecool/trivia/service/LeaderboardService.java
@@ -1,5 +1,6 @@
 package com.codecool.trivia.service;
 
+import com.codecool.trivia.dto.LeaderboardDTO;
 import com.codecool.trivia.logger.ConsoleLogger;
 import com.codecool.trivia.model.entity.TriviaUser;
 import com.codecool.trivia.repository.UserRepository;
@@ -9,6 +10,7 @@ import java.util.List;
 
 @Service
 public class LeaderboardService {
+  private static final int LEADERBOARD_LENGTH = 5;
   private final UserRepository userRepository;
   private final ConsoleLogger logger;
 
@@ -17,7 +19,17 @@ public class LeaderboardService {
     this.logger = logger;
   }
 
-  public List<TriviaUser> findTopFiveUsers() {
-    return userRepository.findTop5ByOrderByPointsDesc();
+  public LeaderboardDTO findTopFiveUsers() {
+    List<TriviaUser> topPlayers = userRepository.findTop5ByOrderByPointsDesc();
+
+    String[] players = new String[LEADERBOARD_LENGTH];
+    double[] points = new double[LEADERBOARD_LENGTH];
+
+    for (int i = 0; i < topPlayers.size(); i++) {
+      players[i] = topPlayers.get(i).getName();
+      points[i] = topPlayers.get(i).getPoints();
+    }
+
+    return new LeaderboardDTO(players, points);
   }
 }
