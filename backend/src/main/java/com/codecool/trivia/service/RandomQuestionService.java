bad05b340df8982eb49a0078282f1a917778c80a
martinbago22 <bageszka17@gmail.com>
2024-02-15 11:24:14 +0100

    add Random to RandomQuestionService, change QuestionDTO to also include question category and difficulty.

--- backend/src/main/java/com/codecool/trivia/service/RandomQuestionService.java
+++ backend/src/main/java/com/codecool/trivia/service/RandomQuestionService.java
@@ -5,14 +5,18 @@ import com.codecool.trivia.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @Service
 public class RandomQuestionService {
     private final QuestionRepository questionRepository;
     private final AnswerRepository answerRepository;
+    private final Random random;
 
     @Autowired
     public RandomQuestionService(QuestionRepository questionRepository, AnswerRepository answerRepository) {
         this.questionRepository = questionRepository;
         this.answerRepository = answerRepository;
+        this.random = new Random();
     }
 }
