fda3a1f6c98bedc96c818addef02db602ca19eaf
martinbago22 <bageszka17@gmail.com>
2024-02-27 14:20:13 +0100

    add spring-starter-security dependency to pom.xml

---
+++ backend/src/main/java/com/codecool/trivia/security/WebSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.codecool.trivia.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable()).cors(cors -> cors.disable())
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("user/**").permitAll()
+                                .requestMatchers("team/**").hasRole("USER")
+                                .requestMatchers("/error").permitAll()
+                                .anyRequest().authenticated()
+
+                );
+
+        http.authenticationProvider(authenticationProvider());
+
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
